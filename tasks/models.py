9e865e1527b74e95b573c8c5e222acf89b7956f8
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-05-31 09:33:37 +0400

    YOLO

--- tasks/models.py
+++ tasks/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    last_name = models.TextField(blank=True, null=True)  
+    last_name = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.username
+
 
 class Task(models.Model):
     STATUS_CHOICES = [
@@ -16,8 +18,10 @@ class Task(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default='new')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return f"{self.title} ({self.status})"
